93c1e9462a0e7b4be6b6e6bca167d19ef509755f
James-Pirie <63281228+James-Pirie@users.noreply.github.com>
2024-07-05 19:04:04 +1200

    Setup react router and rearanged project structure
    Renamed app to home and removed default vite application features.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Home from './components/Home.tsx'
+import NotFound from './components/NotFound.tsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([{
+  path: '/',
+  Component: Home,
+  errorElement: <NotFound/>
+}])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
